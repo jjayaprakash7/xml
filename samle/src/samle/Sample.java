eafdade3b0d3379bacfe44939f0ddb54c9e36d1c
JAYAPRAKASH <JAYAPRAKASH@DESKTOP-8K9LILP>
2020-11-04 10:52:33 +0530

    commit the code c

--- samle/src/samle/Sample.java
+++ samle/src/samle/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 	System.out.println("Hellow World");
-	System.out.println("Hellow World");
+	System.out.println("Hellow World1");
 
 	}
 
